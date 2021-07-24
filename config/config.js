fde571dd02025e7c91dc8795ce099b703f61fa60
Diane <dianekim120@gmail.com>
2021-07-24 11:16:11 -0400

    dotenv

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 module.exports = {
   development: {
     username: 'root',
-    password: 'admin',
+    password: process.env.DB_PASSWORD,
     database: 'webT',
     host: '127.0.0.1',
     dialect: 'mysql',
