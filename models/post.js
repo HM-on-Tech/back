8e8f56844444e317c7411a8c8ce3b58a4b41b9b4
Diane <dianekim120@gmail.com>
2021-09-13 19:35:26 -0400

    publications table

--- models/post.js
+++ models/post.js
@@ -28,5 +28,6 @@ module.exports = class Post extends Model {
   }
   static associate(db) {
     db.Post.belongsTo(db.User)
+    db.Post.belondsTo(db.Publication)
   }
 };
---
+++ routes/publication.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { Post } = require('../models');
+
+
+const router = express.Router();
+
+
+/**
+ * Add InitTable information to the database
+ */
+router.post('/add', async (req, res, next) => {  
+  try {
+    console.log('post add request on the server.')
+    const result = await Publication.create(
+        req.body
+)
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/list', async (req, res, next) => {  
+  try {
+    const result = await Publication.findAll()
+    console.log(result)
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// router.get('/get/:edit', async (req, res, next) => {  
+//   const editId = req.params.edit;
+//   const result = await Post.findByPk(editId)
+//   return res.status(200).json(result);
+// });
+
+// router.post('/edit/:edit', async (req, res, next) => {  
+//   const editId = req.params.edit;
+//   const { title, content, author, thumbnail} = req.body;
+
+  // const result = await Post.update(
+  //   {title: title, content: content, author: author, thumbnail:thumbnail},
+  //   { where: { id: editId}}
+//   )
+//   if (result[0] == 1) {
+//     return res.status(200).json({'result':'success'});
+//   }
+//   return res.status(500).json({'result':'failed'})
+// });
+
+module.exports = router;
