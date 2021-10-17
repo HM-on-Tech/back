d119df17882b1b6e2618864d45ead7bb66421d03
Diane <dianekim120@gmail.com>
2021-10-17 10:42:58 -0400

    end for RDS AWS

--- config/config.js
+++ config/config.js
@@ -18,10 +18,13 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: 'admin',
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DATABASE_NAME,
+    host: process.env.RDS_HOSTNAME,
+    dialect: process.env.RDS_DIALECT,
+    dialectOptions: {
+        ssl: process.env.RDS_SSL
+    }
   },
 };
--- helper/auth.js
+++ helper/auth.js
@@ -8,7 +8,6 @@ exports.authMiddleware = async (req, res, next) => {
   
     if (jwtToken == null || jwtToken == 'null')  return res.json(null);
     const  payload = jwt.verify(jwtToken, process.env.JWT_SECRET);
-  
     const verifiedUser = await User.findOne({where: {id:payload.user.id, email:payload.user.email}});
     req.user = verifiedUser;
     next();
